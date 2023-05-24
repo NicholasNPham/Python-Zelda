5f4dc51e710bfd9d109f459f138db2177df037e6
Nicholas Pham <132528074+NicholasNPham@users.noreply.github.com>
2023-05-24 14:06:33 -0400

    Add files via upload

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ from settings import *
 from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack):
+    def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack, create_magic):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
@@ -32,6 +32,13 @@ class Player(pygame.sprite.Sprite):
         self.weapon_switch_time = None
         self.switch_duration_cooldown = 200
 
+        # Magic
+        self.create_magic = create_magic
+        self.magic_index = 0
+        self.magic = list(magic_data.keys())[self.magic_index]
+        self.can_switch_magic = True
+        self.magic_switch_time = None
+
         # stats
         self.stats = {'health': 100, 'energy': 60, 'attack': 10, 'magic': 4, 'speed': 5}
         self.health = self.stats['health'] * 0.5
@@ -58,19 +65,19 @@ class Player(pygame.sprite.Sprite):
             The player is set a positive x and a positive y. (Y Coords are reversed). -1 means going up. 
             """
             # Movement Input.
-            if keys[pygame.K_w]:
+            if keys[pygame.K_UP]:
                 self.direction.y = -1
                 self.status = 'up'
-            elif keys[pygame.K_s]:
+            elif keys[pygame.K_DOWN]:
                 self.direction.y = 1
                 self.status = 'down'
             else:
                 self.direction.y = 0
 
-            if keys[pygame.K_d]:
+            if keys[pygame.K_RIGHT]:
                 self.direction.x = 1
                 self.status = 'right'
-            elif keys[pygame.K_a]:
+            elif keys[pygame.K_LEFT]:
                 self.direction.x = -1
                 self.status = 'left'
             else:
@@ -86,8 +93,12 @@ class Player(pygame.sprite.Sprite):
             if keys[pygame.K_LCTRL]:
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
-                print('magic')
+                style = list(magic_data.keys())[self.magic_index]
+                strength = list(magic_data.values())[self.magic_index]['strength'] + self.stats['magic']
+                cost = list(magic_data.values())[self.magic_index]['cost']
+                self.create_magic(style, strength, cost)
 
+            # Weapon Switch Handler
             if keys[pygame.K_q] and self.can_switch_weapon:
                 self.can_switch_weapon = False
                 self.weapon_switch_time = pygame.time.get_ticks()
@@ -98,6 +109,18 @@ class Player(pygame.sprite.Sprite):
                     self.weapon_index = 0
 
                 self.weapon = list(weapon_data.keys())[self.weapon_index]
+
+            # Magic Switch Handler
+            if keys[pygame.K_e] and self.can_switch_magic:
+                self.can_switch_magic = False
+                self.magic_switch_time = pygame.time.get_ticks()
+
+                if self.magic_index < len(list(magic_data.keys())) - 1:
+                    self.magic_index += 1
+                else:
+                    self.magic_index = 0
+
+                self.magic = list(magic_data.keys())[self.magic_index]
 
     def get_status(self):
         # Idle Status.
@@ -155,9 +178,15 @@ class Player(pygame.sprite.Sprite):
                 self.attacking = False
                 self.destroy_attack()
 
+        # Weapon Cooldown
         if not self.can_switch_weapon:
             if current_time - self.weapon_switch_time > self.switch_duration_cooldown:
                 self.can_switch_weapon = True
+
+        # Magic Cooldown
+        if not self.can_switch_magic:
+            if current_time - self.magic_switch_time > self.switch_duration_cooldown:
+                self.can_switch_magic = True
 
     def animate(self):
         animation = self.animations[self.status]
