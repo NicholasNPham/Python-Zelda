33b1d0f9fbd238b9c73ab52f8bb7b9b3e9f373cb
Nicholas Pham <132528074+NicholasNPham@users.noreply.github.com>
2023-06-01 19:18:22 -0400

    Add files via upload

--- level.py
+++ level.py
@@ -9,12 +9,14 @@ from ui import UI
 from enemy import Enemy
 from particles import AnimationPlayer
 from magic import MagicPlayer
+from upgrade import Upgrade
 
 class Level:
     def __init__(self):
 
         # Get the display surface.
         self.display_surface = pygame.display.get_surface()
+        self.game_paused = False
 
         # Sprite Group Setup; Allows to hold and manage multiple Sprite objects. This Case 2.
         self.visible_sprites = YSortCameraGroup()
@@ -30,6 +32,7 @@ class Level:
 
         # User Interface
         self.ui = UI()
+        self.upgrade = Upgrade(self.player)
 
         # Particles.
         self.animation_player = AnimationPlayer()
@@ -87,7 +90,8 @@ class Level:
                                     [self.visible_sprites, self.attackable_sprites],
                                     self.obstacle_sprites,
                                     self.damage_layer,
-                                    self.trigger_death_particles)
+                                    self.trigger_death_particles,
+                                    self.add_exp)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
@@ -130,13 +134,24 @@ class Level:
     def trigger_death_particles(self, pos, particle_type):
         self.animation_player.create_particles(particle_type, pos, self.visible_sprites)
 
+    def add_exp(self, amount):
+
+        self.player.exp += amount
+
+    def toggle_menu(self):
+
+        self.game_paused = not self.game_paused
+
     def run(self):
-        # Update and draw the game.
         self.visible_sprites.custom_draw(self.player)
-        self.visible_sprites.update()
-        self.visible_sprites.enemy_update(self.player)
-        self.player_attack_logic()
         self.ui.display(self.player)
+
+        if self.game_paused:
+            self.upgrade.display()
+        else:
+            self.visible_sprites.update()
+            self.visible_sprites.enemy_update(self.player)
+            self.player_attack_logic()
 
 # "CameraGroup" Functionalize the camera. "YSort" means sorting the sprites on the y coordinate.
 class YSortCameraGroup(pygame.sprite.Group):
